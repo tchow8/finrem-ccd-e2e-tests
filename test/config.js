d3127801811d740659048467435a3d733db1d8e8
vianvi <30834324+vianvi@users.noreply.github.com>
2018-07-31 16:27:27 +0100

    add case worker search

--- test/config.js
+++ test/config.js
@@ -2,8 +2,8 @@ module.exports = {
   TestFrontendUrl: process.env.TEST_E2E_URL || 'https://www.ccd.demo.platform.hmcts.net/',
   TestSolicitorUserName: process.env.TEST_SOLS_USER_NAME || 'vivekreddyfinremsol@mailinator.com',
   TestSolicitorPassword: process.env.TEST_SOLS_PASSWORD || 'Testing01',
-  TestCaseWorkerUserName: process.env.TEST_CASEWORKER_USER_NAME || '',
-  TestCaseWorkerPassword: process.env.TEST_CASEWORKER_PASSWORD || '',
+  TestCaseWorkerUserName: process.env.TEST_CASEWORKER_USER_NAME || 'vivek_fr_courtadmn@mailinator.com',
+  TestCaseWorkerPassword: process.env.TEST_CASEWORKER_PASSWORD || 'London01',
   TestJudgeUserName: process.env.TEST_CASEWORKER_USER_NAME || '',
   TestJudgePassword: process.env.TEST_CASEWORKER_PASSWORD || '',
   links: {
--- test/end2end/journeys/e2eJourney.js
+++ test/end2end/journeys/e2eJourney.js
@@ -23,14 +23,13 @@ Scenario('Verify Solicitors Happypath Scenario', I => {
 });
 
 
-xScenario('Verify Court Admin update case Scenario', I => {
+Scenario('Verify Court Admin update case Scenario', I => {
   I.signinIdam(testConfig.TestCaseWorkerUserName, testConfig.TestCaseWorkerPassword);
-  I.updateCase();
-  I.updateSolAddress();
-  I.see('Update Order Application');
-  I.see('Financial Remedy COURT ADMIN');
-  I.click('Solicitor');
-  I.see('103 PF');
+  I.wait(2);
+  I.searchCase(solRef);
+  I.wait(5);
+  I.click('a div.text-16');
+  I.wait(2);
 });
 
 xScenario('Verify Court judge approve case', I => {
