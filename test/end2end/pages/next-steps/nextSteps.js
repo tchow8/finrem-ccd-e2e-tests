bad8f51a9280eedefa0f5d4c90573540228e33ea
mahesh <mahesh.goduguchinta@hmcts.net>
2019-02-25 13:51:55 +0000

    added tests for HWF

--- test/end2end/pages/next-steps/nextSteps.js
+++ test/end2end/pages/next-steps/nextSteps.js
@@ -10,7 +10,7 @@ function searchCase(reference) {
   I.fillField('input[id="solicitorReference"]', 'AUTO-'+reference);
   I.wait(5);
   I.click('Apply');
-  I.wait(50);
+  I.wait(40);
   I.click({ xpath: '//*[@id="search-result"]/ccd-search-result/table/tbody/tr/td[1]/a/ccd-field-read/ccd-field-read-label/ccd-read-number-field' });
   I.wait(10);
 
@@ -25,39 +25,6 @@ function optionsPage(){
   I.wait(5);
 }
 
-function updateCase() {
-  const I = this;
-  I.wait(5);
-  I.selectOption('select.form-control.ccd-dropdown.EventTrigger-empty.ng-untouched.ng-pristine.ng-invalid', 'Issue Application');
-  I.wait(5);
-  I.click('Go');
-  I.wait(5);
-  I.fillField('input[id="issueDate-day"]', '1');
-  I.fillField('input[id="issueDate-month"]', '2');
-  I.fillField('input[id="issueDate-year"]', '2000');
-  I.click('Continue');
-  I.wait(10);
-  I.click('Submit');
-  I.wait(20);
-  I.selectOption('select.form-control.ccd-dropdown.EventTrigger-empty.ng-untouched.ng-pristine.ng-invalid', 'Assign To Judge');
-  I.wait(10);
-  I.click('Go');
-  I.wait(10);
-  I.selectOption('select[id="assignedToJudgeReason"]', 'Draft consent order');
-  I.wait(10);
-  I.selectOption('select[id="assignedToJudge"]', 'nasim_fr_judge@mailinator.com');
-  I.wait(10);
-  I.fillField('input[id="referToJudgeDate-day"]', '1');
-  I.fillField('input[id="referToJudgeDate-month"]', '2');
-  I.fillField('input[id="referToJudgeDate-year"]', '2018');
-  I.wait(10);
-  I.fillField('input[id="referToJudgeText"]', 'Please look into this case on urgent basis ');
-  I.click('Continue');
-  I.wait(10);
-  I.click('Submit');
-  I.wait(10);
-
-}
 
 function approveApplication() {
   const I = this;
@@ -85,4 +52,4 @@ function approveApplication() {
 }
 
 
-module.exports = { searchCase, updateCase, approveApplication, optionsPage};
+module.exports = { searchCase, approveApplication, optionsPage};
--- test/end2end/pages/payment-page/paymentPage.js
+++ test/end2end/pages/payment-page/paymentPage.js
@@ -1,11 +1,12 @@
 function paymentPage() {
 
-const I = this;
-I.wait(2);
-I.checkOption('input[id="helpWithFeesQuestion-No"]');
-I.wait(2);
-I.click('Continue');
-I.wait(5);
+  const I = this;
+  I.wait(2);
+  //I.checkOption('input[id="helpWithFeesQuestion-No"]');
+  I.checkOption('input[id="helpWithFeesQuestion-Yes"]');
+  I.wait(2);
+  I.click('Continue');
+  I.wait(5);
 }
 
 module.exports = { paymentPage };
