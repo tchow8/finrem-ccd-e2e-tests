f4dd981a969765058c1ea5a5760e9b38341d2ae8
devadathpt <devadathpt@gmail.com>
2019-04-09 14:44:39 +0100

    modified codecept.conf.js

---
+++ test/end2end/codecept.Pba.conf.js
@@ -0,0 +1,36 @@
+const testConfig = require('test/config.js');
+
+exports.config = {
+
+ tests: './**/**/e2ePbaJourney.js',
+  output: `${process.cwd()}/functional-output`,
+  helpers: {
+    Puppeteer: {
+      url: testConfig.TestFrontendUrl || 'https://www.ccd.demo.platform.hmcts.net/',
+      waitForTimeout: 5000,
+      waitForAction: 2000,
+      getPageTimeout: 30000,
+      show: false,
+      waitForNavigation: 'networkidle0',
+      ignoreHTTPSErrors: true,
+      headless: true,
+      chrome: {
+        ignoreHTTPSErrors: true,
+        args: [
+          '--no-sandbox',
+          '--proxy-server=proxyout.reform.hmcts.net:8080'
+        ]
+      }
+
+    }
+  },
+  include: { I: './pages/steps.js' },
+  mocha: {
+    reporterOptions: {
+      reportDir: process.env.E2E_OUTPUT_DIR || './functional-output',
+      reportName: 'FinremSolTests',
+      inlineAssets: true
+    }
+  },
+  name: 'Finrem Sol Tests'
+};
