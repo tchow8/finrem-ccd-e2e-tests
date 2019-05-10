673ef5fd979c3025133bf0803aacadc0afd36934
devadathpt <devadathpt@gmail.com>
2019-05-10 12:13:09 +0100

    testing vault access for signin details

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   TestFrontendUrl: process.env.TEST_E2E_URL || 'https://www-ccd.aat.platform.hmcts.net/',
-  TestSolicitorUserName: process.env.TEST_SOLS_USER_NAME,
-  TestSolicitorPassword: process.env.TEST_SOLS_PASSWORD,
+  TestSolicitorUserName: process.env.TEST_SOLS_USER_NAME || 'mahesh_fr_sol@mailinator.com',
+  TestSolicitorPassword: process.env.TEST_SOLS_PASSWORD || 'London01',
   TestCaseWorkerUserName: process.env.TEST_CASEWORKER_USER_NAME || 'mahesh_fr_courtadmn@mailinator.com',
   TestCaseWorkerPassword: process.env.TEST_CASEWORKER_PASSWORD || 'London01',
   TestJudgeUserName: process.env.TEST_JUDGE_USER_NAME || 'mahesh_fr_judge@mailinator.com',
--- test/end2end/Test.js
+++ test/end2end/Test.js
@@ -1,10 +1,23 @@
-Feature('finrem  e2e HWF Journey');
+Feature('Test Sign using Vault ');
 const testConfig = require('test/config.js');
 const dateUtil = require('test/end2end/helpers/dateUtil.js');
 const solRef = dateUtil.createSolicitorReference();
 const pbaValue = false;
 
-Scenario('Verify Solicitors Happypath Scenario1', I => {
+Scenario('Verify Solicitors Login Scenario1', I => {
   I.signinIdam(testConfig.TestSolicitorUserName, testConfig.TestSolicitorPassword);
-  I.wait(10);
-});
+  console.log('Sol User name is ' + testConfig.TestSolicitorUserName);
+  console.log('Sol Password is ' + testConfig.TestSolicitorPassword);
+  });
+
+Scenario('Verify CaseWorker Login Scenario1', I => {
+  I.signinIdam(testConfig.TestCaseWorkerUserName, testConfig.TestCaseWorkerPassword);
+  console.log('caseworker User name is ' + testConfig.TestCaseWorkerUserName);
+  console.log('caseworker Password is ' + testConfig.TestCaseWorkerPassword);
+ });
+
+Scenario('Verify Judge Login Scenario1', I => {
+  I.signinIdam(testConfig.TestJudgeUserName, testConfig.TestJudgePassword);
+  console.log('Judge User name is ' + testConfig.TestJudgeUserName);
+  console.log('Judge Password is ' + testConfig.TestJudgePassword);
+ });
