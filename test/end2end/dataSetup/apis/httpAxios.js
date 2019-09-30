16a6f83dcc4d58b438afa15ce7f04e5757ae1f30
Sreekanth <sreekanth.puligadda@hmcts.net>
2019-09-30 18:33:48 +0100

    axios proxy fixed

--- test/end2end/dataSetup/apis/httpAxios.js
+++ test/end2end/dataSetup/apis/httpAxios.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 const tough = require('tough-cookie');
 
+let httpsProxyAgent = require('https-proxy-agent');
+const agent = new httpsProxyAgent('http://proxyout.reform.hmcts.net:8080'); 
 
 const httpAxios = axios.create({
   withCredentials: true,
@@ -12,12 +14,13 @@ const httpAxios = axios.create({
 axiosCookieJarSupport(httpAxios);
 httpAxios.defaults.jar = new tough.CookieJar();
 
-// httpAxios.interceptors.request.use(function (config) {
-// //   console.log(config.method+' -> '+config.url);
-//   return config;
-// }, function (error) {
-//   return Promise.reject(error);
-// });
+httpAxios.interceptors.request.use(function (config) {
+  config.httpsAgent = agent; 
+//   console.log(config.method+' -> '+config.url);
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
 
 // // Add a response interceptor
 // httpAxios.interceptors.response.use(function (response) {
