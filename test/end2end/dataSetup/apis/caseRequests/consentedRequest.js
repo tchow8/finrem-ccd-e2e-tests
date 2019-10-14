d84b91a4d0c167bf08db773948f4573b1e4ff7e7
Sreekanth <sreekanth.puligadda@hmcts.net>
2019-10-14 09:20:23 +0100

    Consented using prod version case request

--- test/end2end/dataSetup/apis/caseRequests/consentedRequest.js
+++ test/end2end/dataSetup/apis/caseRequests/consentedRequest.js
@@ -1,5 +1,5 @@
-
-var caseRequest = {
+// AAT Version
+var caseRequest_disabled = {
   'data': {
     'applicantRepresented':'Yes',
     'isAdmin': 'No',
@@ -105,5 +105,103 @@ var caseRequest = {
   'ignore_warning': false,
   'draft_id': null
 };
+
+
+
+
+// PROD version
+var caseRequest = {
+  'data': {
+    'solicitorName': 'Evangeline Ochoa',
+    'solicitorFirm': 'Willow Hull',
+    'solicitorReference': 'Itaque itaque eligen',
+    'solicitorAddress': {
+      'AddressLine1': 'Apartment 1',
+      'AddressLine2': '15 Jermyn Street',
+      'AddressLine3': '',
+      'PostTown': 'London',
+      'County': '',
+      'PostCode': 'SW1Y 6LT',
+      'Country': 'United Kingdom'
+    },
+    'solicitorPhone': '+1 (604) 491-6975',
+    'solicitorEmail': 'syjenyjuje@mailinator.com',
+    'solicitorDXnumber': '617',
+    'solicitorAgreeToReceiveEmails': 'No',
+    'divorceCaseNumber': 'LV18D84321',
+    'divorceStageReached': 'Decree Nisi',
+    'divorceDecreeNisiDate': '2019-10-10',
+    'divorceUploadEvidence1': {
+      'document_url': 'http://dm-store-aat.service.core-compute-aat.internal/documents/411c637d-69c7-4346-9e34-de98e8912a59',
+      'document_binary_url': 'http://dm-store-aat.service.core-compute-aat.internal/documents/411c637d-69c7-4346-9e34-de98e8912a59/binary',
+      'document_filename': 'dummy.pdf'
+    },
+    'applicantFMName': 'Knox Vaughn',
+    'applicantLName': 'Becker',
+    'appRespondentFMName': 'Sharon Spears',
+    'appRespondentLName': 'Merritt',
+    'appRespondentRep': 'Yes',
+    'rSolicitorName': 'Idola Drake',
+    'rSolicitorFirm': 'Officiis voluptas ex',
+    'rSolicitorReference': 'Tenetur nostrud nesc',
+    'rSolicitorAddress': {
+      'AddressLine1': '58 Ferry Road',
+      'AddressLine2': '',
+      'AddressLine3': '',
+      'PostTown': 'London',
+      'County': '',
+      'PostCode': 'SW13 9PW',
+      'Country': 'United Kingdom'
+    },
+    'rSolicitorPhone': '+1 (279) 105-7388',
+    'rSolicitorEmail': 'rudiz@mailinator.com',
+    'rSolicitorDXnumber': '790',
+    'natureOfApplication2': [
+      'Property Adjustment  Order',
+      'A settlement or a transfer of property',
+      'Pension Compensation Attachment Order',
+      'Pension Compensation Sharing Order',
+      'Pension Attachment Order',
+      'Pension Sharing Order',
+      'Lump Sum Order',
+      'Periodical Payment Order'
+    ],
+    'natureOfApplication3a': 'Velit dignissimos vo',
+    'natureOfApplication3b': 'Officiis cupiditate',
+    'orderForChildrenQuestion1': 'Yes',
+    'natureOfApplication5': 'No',
+    'natureOfApplication6': [
+      'Other',
+      'When not habitually resident',
+      'training',
+      'disability expenses',
+      'In addition to child support',
+      'Step Child or Step Children'
+    ],
+    'natureOfApplication7': 'Delectus accusamus',
+    'consentOrder': {
+      'document_url': 'http://dm-store-aat.service.core-compute-aat.internal/documents/4b4e365b-636d-4084-aa90-ce80e28df47b',
+      'document_binary_url': 'http://dm-store-aat.service.core-compute-aat.internal/documents/4b4e365b-636d-4084-aa90-ce80e28df47b/binary',
+      'document_filename': 'dummy.pdf'
+    },
+    'd81Question': 'Yes',
+    'd81Joint': {
+      'document_url': 'http://dm-store-aat.service.core-compute-aat.internal/documents/f611c39f-006b-41ac-900b-13508a253ebe',
+      'document_binary_url': 'http://dm-store-aat.service.core-compute-aat.internal/documents/f611c39f-006b-41ac-900b-13508a253ebe/binary',
+      'document_filename': 'dummy.pdf'
+    },
+    'pensionCollection': [],
+    'otherCollection': []
+  },
+  'event': {
+    'id': 'FR_solicitorCreate',
+    'summary': '',
+    'description': ''
+  },
+  'event_token': 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJnMW12bjczYnUwN2oxZmZiaTh0OWwyM3JwayIsInN1YiI6IjEyMGIzNjY1LTBiOGEtNGU4MC1hY2UwLTAxZDhkNjNjMTAwNSIsImlhdCI6MTU3MTA0MDc5NCwiZXZlbnQtaWQiOiJGUl9zb2xpY2l0b3JDcmVhdGUiLCJjYXNlLXR5cGUtaWQiOiJGaW5hbmNpYWxSZW1lZHlNVlAyIiwianVyaXNkaWN0aW9uLWlkIjoiRElWT1JDRSIsImNhc2UtdmVyc2lvbiI6ImJmMjFhOWU4ZmJjNWEzODQ2ZmIwNWI0ZmEwODU5ZTA5MTdiMjIwMmYifQ.CaymYn85erCwS3HsWQC-XICQ7plSYZGm0J2NZPsUDE8',
+  'ignore_warning': false,
+  'draft_id': null
+};
+
 
 exports.default = caseRequest;
